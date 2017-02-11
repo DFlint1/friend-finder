7a7820911461eedb0354358c180627623a756e82
Dana Flint <Dana_Flint@msn.com>
2017-02-10 23:34:10 -0600

    adding more content

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,2 +1,12 @@
 //friends.js
 //This file will be used to display JSON of all possible friends
+
+var friendData= [
+	{
+		friendName: "Julia Gulia",
+		friendPhoto: "http://thoughtcatalog.files.wordpress.com/2014/01/screen-shot-2014-01-22-at-11-21-05-pm.png?w=584&h=389",
+		friendScores: [4,4,4,4,4,4,4,4,4,4],
+	}
+	];
+
+	module.exports = friendData;
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,1 +1,37 @@
 //apiroutes.js
+//This is a get route with url/api/friends to display a json of all possible friends
+//This links routes to all data sources
+
+var friendData = require ('../data/friends.js');
+var path = require('path');
+
+
+//Routes
+//==========================================================
+module.exports = function(app){
+	app.get('/api/friends', function(req,res){
+	rest.json(friendData);
+	})
+
+	app.post('/api/friends', function(req, res){
+		var lowestDifferenceInt = 50;
+		var chosenMatch;
+		friendData.forEach(function(storedUserObject){
+			var difference = 0;
+			for(i=0;i<storedUserObject.friendScores.length;i++){
+				difference+=Math.abs(storedUserObject.friendScores[i] - req.body.friendScores[i]);
+			} if(difference<lowestDifferenceInt){
+				lowestDifferenceInt = difference;
+				chosenMatch = storedUserObject;
+			}
+		});
+
+		res.json(chosenMatch);
+		friendData.push(req.body);
+		app.post('/api/clear', function(req, res){
+		// Empty out the array of data
+		friendData = [];
+
+		console.log(friendData);
+	})
+}
